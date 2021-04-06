e9df4716d691f76791d179b1027462565e96d6a7
JonathanScrok <jonathanwscrok@gmail.com>
2021-04-06 19:26:45 -0300

    Atualização PrincipalMain para organização em métodos e criação de comentários, para explicação dos mesmos.

--- Calculadora/PrincipalMain.cs
+++ Calculadora/PrincipalMain.cs
@@ -6,49 +6,59 @@ namespace Calculadora
     {
         public static int Numero;
         public static char operacao;
-
+        public static string novamente;
         static void Main(string[] args)
         {
-            string novamente;
             Console.WriteLine("Olá, tudo bem? oque gostaria de fazer?");
             do
             {
-                Console.WriteLine("");
-                Console.WriteLine("| Tabuada ou Calculadora |");
-                string escolha = Console.ReadLine();
+                Inicio(); //Chama o método de Inicio, para começar o programa
 
-                if (escolha.Equals("Tabuada", StringComparison.CurrentCultureIgnoreCase))
-                {
-                    Tabuada.TabuadaGeral();
-                }
-                else if (escolha.Equals("Calculadora", StringComparison.CurrentCultureIgnoreCase))
-                {
-                    Calculador.CalculadoraGeral();
-                }
-                else
-                {
-                    Console.WriteLine("");
-                    Console.WriteLine("Não entendi, escolha uma opção correta por favor!");
-                }
-
-
-                // ------------------------------- TENTAR NOVAMENTE ---------------------------------------
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("Quer ir novamente? (S/N)");
-                // Lê a escolha , se quer tentar novamente ou não...
-                novamente = Console.ReadLine();
-
-                // Limpa a tela
-                Console.Clear();
-
-                // Se a variavel (novamente) for igual a "S" ou "s" declara ela como minúscula
-                if (novamente.Equals("S", StringComparison.CurrentCultureIgnoreCase))
-                {
-                    novamente = "s";
-                }
+                ValidaTentarNovamente(); //Chama o método de Inicio, para começar o programa
 
             } while (novamente == "s"); //Enquanto a variavel (novamente) for igual a "s", reinicia o processo, se não.. Finaliza!
+        }
+
+        private static void Inicio()
+        {
+            Console.WriteLine("");
+            // Escolha de Tabuada ou Calculadora
+            Console.WriteLine("| Tabuada ou Calculadora |");
+            string escolha = Console.ReadLine();
+
+
+            if (escolha.Equals("Tabuada", StringComparison.CurrentCultureIgnoreCase)) //Se for escolhido Tabuada chamar o metodo TabuadaGeral()
+            {
+                Tabuada.TabuadaGeral();
+            }
+            else if (escolha.Equals("Calculadora", StringComparison.CurrentCultureIgnoreCase)) //Se for escolhido Calculadora chamar o metodo CalculadoraGeral();
+            {
+                Calculador.CalculadoraGeral();
+            }
+            else //Se não for escolhida uma opção válida
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Não entendi, escolha uma opção correta por favor!");
+                Inicio();
+            }
+        }
+
+        public static void ValidaTentarNovamente()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Quer ir novamente? (S/N)");
+            // Lê a escolha , se quer tentar novamente ou não...
+            novamente = Console.ReadLine();
+
+            // Limpa a tela
+            Console.Clear();
+
+            // Se a variavel (novamente) for igual a "S" ou "s" declara ela como minúscula
+            if (novamente.Equals("S", StringComparison.CurrentCultureIgnoreCase))
+            {
+                novamente = "s";
+            }
         }
     }
 }
